c1860566ec7896bd3db92c5581ec574b680ec546
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-05-09 22:36:11 +0545

    feat: Add username field to registration form

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,15 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
 using WebApplication6.Areas.Identity.Data;
 
 namespace WebApplication6.Areas.Identity.Pages.Account
@@ -21,6 +21,7 @@ namespace WebApplication6.Areas.Identity.Pages.Account
         private readonly UserManager<CustomUser> _userManager;
         private readonly IUserStore<CustomUser> _userStore;
         private readonly IUserEmailStore<CustomUser> _emailStore;
+        // private readonly IUserRoleStore<CustomUser> _roleStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
@@ -29,6 +30,7 @@ namespace WebApplication6.Areas.Identity.Pages.Account
             IUserStore<CustomUser> userStore,
             SignInManager<CustomUser> signInManager,
             ILogger<RegisterModel> logger,
+            // IUserRoleStore<CustomUser> roleStore,
             IEmailSender emailSender)
         {
             _userManager = userManager;
@@ -36,6 +38,7 @@ namespace WebApplication6.Areas.Identity.Pages.Account
             _emailStore = GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
+            // _roleStore = roleStore;
             _emailSender = emailSender;
         }
 
@@ -73,6 +76,9 @@ namespace WebApplication6.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Required]
+            public string UserName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -108,8 +114,11 @@ namespace WebApplication6.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                // await _roleStore.SetRoleAsync(user, "Blogger", CancellationToken.None);
+                // user.UserName = Input.UserName;
+                user.Role = "Blogger";
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
